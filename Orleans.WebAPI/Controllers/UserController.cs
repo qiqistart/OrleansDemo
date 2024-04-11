0b2db6179aa42ecb44fcbb1e1fbe8a8459481008
hzt <84691873@qq.com>
2024-04-11 10:08:05 +0800

    修改文件

--- Orleans.WebAPI/Controllers/UserController.cs
+++ Orleans.WebAPI/Controllers/UserController.cs
@@ -1,19 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Orleans.WebAPI.Controllers
+namespace Orleans.WebAPI.Controllers;
+
+/// <summary>
+/// 
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
 
-        public UserController() { }
+    public UserController() { }
 
 
-         
-    }
+
 }
