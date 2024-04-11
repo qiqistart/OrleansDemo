31fd12eeeb7412ba218276f927339a9b56bdb1e9
huzitao <84691873@qq.com>
2024-04-11 12:35:33 +0800

    新增部分类

---
+++ Orleans.Infrastructure/InfrantructureRegister.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans.Infrastructure.Repository.User;
+
+namespace Orleans.Infrastructure;
+
+/// <summary>
+/// 
+/// </summary>
+public static class InfrantructureRegister
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="service"></param>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    public static IServiceCollection addInfrantructure(this IServiceCollection service, IConfiguration configuration)
+    {
+        service.AddDbContext<OrleansDbContext>(opt =>
+            {
+                opt.UseMySQL(configuration.GetConnectionString("AdminConnectionStrings"));
+            }
+        );
+        service.AddScoped<ISysUserRepository, SysUserRepository>();
+        return service;
+    }
+}
