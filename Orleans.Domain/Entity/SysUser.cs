12f660b9773f7374cb146bcc1e717a1d4f210545
hzt <84691873@qq.com>
2024-04-11 10:10:26 +0800

    修改文件

--- Orleans.Domain/Entity/SysUser.cs
+++ Orleans.Domain/Entity/SysUser.cs
@@ -5,28 +5,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Orleans.Domain.Entity
+namespace Orleans.Domain.Entity;
+public class SysUser : BaseIdEntity<string>
 {
-    public class SysUser : BaseIdEntity<string>
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public string UserName { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string Account { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string UserName { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Account { get; set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public string Avatar { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Avatar { get; set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public string PassWord { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string PassWord { get; set; }
 
-    }
 }
--- Orleans.WebAPI/Identity/SystemUserGrantValidator.cs
+++ Orleans.WebAPI/Identity/SystemUserGrantValidator.cs
@@ -2,37 +2,35 @@
 using IdentityServer4.Validation;
 using System.Security.Claims;
 
-namespace Orleans.WebAPI.Identity
+namespace Orleans.WebAPI.Identity;
+public class SystemUserGrantValidator : IExtensionGrantValidator
 {
-    public class SystemUserGrantValidator : IExtensionGrantValidator
-    {
-        public string GrantType => "SystemUser";
+    public string GrantType => "SystemUser";
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public async Task ValidateAsync(ExtensionGrantValidationContext context)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public async Task ValidateAsync(ExtensionGrantValidationContext context)
+    {
+        var clientId = context.Request.Raw.Get("client_id");
+        var userId = context.Request.Raw.Get("userId");
+        if (!clientId.Equals("Admin"))
         {
-            var clientId = context.Request.Raw.Get("client_id");
-            var userId = context.Request.Raw.Get("userId");
-            if (!clientId.Equals("Admin"))
-            {
-                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "错误的客户端id");
-                return;
-            }
-            if (string.IsNullOrEmpty(userId))
-            {
-                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "用户信息丢失！");
-                return;
-            }
-            var claim = new Claim[]
-            {
-                new Claim("UserId", userId)
-             };
-            context.Result = new GrantValidationResult(userId, GrantType, claim);
+            context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "错误的客户端id");
             return;
         }
+        if (string.IsNullOrEmpty(userId))
+        {
+            context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "用户信息丢失！");
+            return;
+        }
+        var claim = new Claim[]
+        {
+                new Claim("UserId", userId)
+         };
+        context.Result = new GrantValidationResult(userId, GrantType, claim);
+        return;
     }
 }
